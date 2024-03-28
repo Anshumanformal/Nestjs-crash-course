70b3a32145a1093c06dba7fa7bb553a559f6f437
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2024-03-28 16:10:27 +0530

    Update keys.ts

--- nest-rest-api/src/config/keys.ts
+++ nest-rest-api/src/config/keys.ts
@@ -1,4 +1,3 @@
 export default {
-    // mongodbURI: "mongodb://anshumanranjan1998:eyrealme123@cluster0.i0nhcav.mongodb.net:27017/nestjsdb"
-    mongodbURI: "mongodb+srv://anshumanranjan1998:eyrealme123@cluster0.i0nhcav.mongodb.net/nestjsdb?retryWrites=true"
+    mongodbURI: ""
 }
